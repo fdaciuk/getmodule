d381289ffdaf5e4b5a5223d71c46d76a07fe6761
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-04 20:15:26 -0300

    Fix indentation

--- tests/getmoduleTest.js
+++ tests/getmoduleTest.js
@@ -6,16 +6,16 @@ require( 'should' );
 var sumService = require( './js/services/sumService' );
 
 describe( 'GETMODULE', function() {
-    it( '"global" should have a "getmodule" property', function() {
-        global.should.have.ownProperty( 'getmodule' );
-    });
+  it( '"global" should have a "getmodule" property', function() {
+    global.should.have.ownProperty( 'getmodule' );
+  });
 
-    it( '"global.getmodule" should be type "function"', function() {
-        getmodule.should.be.type( 'function' );
-    });
+  it( '"global.getmodule" should be type "function"', function() {
+    getmodule.should.be.type( 'function' );
+  });
 
-    it( 'sumService (that requires "sum" module using "getmodule") should be return 1 + 2 = 3', function() {
-        var response = sumService( 1, 2 );
-        response.should.be.equal( 3 );
-    });
+  it( 'sumService (that requires "sum" module using "getmodule") should be return 1 + 2 = 3', function() {
+    var response = sumService( 1, 2 );
+    response.should.be.equal( 3 );
+  });
 });
